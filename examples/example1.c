6c5eb8ad5cc58bfb1640c2f8b71a31ac81cca3ec
pgen <p.gen.progs@gmail.com>
2020-03-28 19:15:27 +0100

    Add an example using two contexts

---
+++ examples/example1.c
@@ -0,0 +1,69 @@
+#include "../ctxopt.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Callback functions */
+/* ****************** */
+
+void
+user_action(char * ctx_name, char * opt_name, char * param, int nb_values,
+            char ** values, int nb_opt_data, void ** opt_data, int nb_ctx_data,
+            void ** ctx_data)
+{
+  printf("User %s\n", values[0]);
+}
+
+void
+groups_action(char * ctx_name, char * opt_name, char * param, int nb_values,
+              char ** values, int nb_opt_data, void ** opt_data,
+              int nb_ctx_data, void ** ctx_data)
+{
+  int v;
+
+  printf("Groups %s", values[0]);
+
+  for (v = 1; v < nb_values; v++)
+    printf(", %s", values[v]);
+
+  printf(".\n");
+}
+
+/* Program entry */
+/* ************* */
+
+int
+main(int argc, char * argv[])
+{
+  int     nb_rem_args = 0;    /* Nb of remaining unprocessed arguments. */
+  char ** rem_args    = NULL; /* Remaining arguments string array.      */
+
+  /* initialize cop */
+  /* """""""""""""" */
+  ctxopt_init(argv[0]);
+
+  /* Create a new context with its  allowed options. */
+  /* """"""""""""""""""""""""""""""""""""""""""""""" */
+  ctxopt_new_ctx("main", "[user>ctx1... #<string>]");
+  ctxopt_new_ctx("ctx1", "group #<string>...");
+
+  /* Attach parameters to the name option */
+  /* """""""""""""""""""""""""""""""""""" */
+  ctxopt_add_opt_settings(parameters, "user", "-u -user");
+  ctxopt_add_opt_settings(parameters, "group", "-g -groups");
+
+  /* Attach a callback action to the name option */
+  /* """"""""""""""""""""""""""""""""""""""""""" */
+  ctxopt_add_opt_settings(actions, "user", user_action, NULL);
+  ctxopt_add_opt_settings(actions, "group", groups_action, NULL);
+
+  /* Parse and check the command line options */
+  /* """""""""""""""""""""""""""""""""""""""" */
+  ctxopt_analyze(argc - 1, argv + 1, &nb_rem_args, &rem_args);
+
+  /* Execute the call back actions in sequence */
+  /* """"""""""""""""""""""""""""""""""""""""" */
+  ctxopt_evaluate();
+
+  exit(EXIT_SUCCESS);
+}
